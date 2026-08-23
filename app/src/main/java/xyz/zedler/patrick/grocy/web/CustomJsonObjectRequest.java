39f98a32f352121ddc9626da4922b6e05123a492
Meganitrospeed <meganitrospeed@gmail.com>
2026-08-23 22:34:10 +0100

    Detect expired proxy sessions in API responses

--- app/src/main/java/xyz/zedler/patrick/grocy/web/CustomJsonObjectRequest.java
+++ app/src/main/java/xyz/zedler/patrick/grocy/web/CustomJsonObjectRequest.java
@@ -83,6 +83,9 @@ public class CustomJsonObjectRequest extends JsonObjectRequest {
           response.data,
           HttpHeaderParser.parseCharset(response.headers, PROTOCOL_CHARSET)
       );
+      if (ReverseProxyAuthManager.handleResponse(url, jsonString)) {
+        return Response.error(new com.android.volley.AuthFailureError());
+      }
       JSONObject result = null;
       if (jsonString.length() > 0) {
         result = new JSONObject(jsonString);
